6de625305a2b2f7dcd72c4307ec71ce3a76f8e5d
Sk Rahamatullah <skrahamatullah500@gmail.com>
2025-05-27 08:51:56 +0530

    project setup ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}', // update based on your project
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+    
