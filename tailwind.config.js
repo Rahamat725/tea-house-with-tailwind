4bfbea5621c938484993f6254af68e88e2a59c25
Sk Rahamatullah <skrahamatullah500@gmail.com>
2025-07-01 20:43:21 +0530

    added news and event

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-    content: [
-      './src/**/*.{html,js,jsx,ts,tsx}', // update based on your project
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
   
-    
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
